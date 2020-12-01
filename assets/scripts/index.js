2b9a5146017c130522b5bfb90d5c05d63355b7b0
rhysseddon <rhysseddon@hotmail.com>
2020-12-01 13:06:01 +0000

    Add shuffles answers function to index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,29 @@
+
+// This code is for start pop up modal
 $(document).ready(function() {
 $("#startModal").modal('show');
-    });
+ });
+
+// Below code shuffles answers 
+ let answers = ["Wales", "Scotland", "Ireland", "England"];
+  function shuffleArray(answers) {
+      
+      for (let i = answers.length -1; i > 0; i--) {
+         j = Math.floor(Math.random() * (i + 1));
+         [answers[i], answers[j]] = [answers[j], answers[i]];
+     }
+ }
+ shuffleArray(answers);
+$(".answer1").text(answers[0]);
+$(".answer2").text(answers[1]);
+$(".answer3").text(answers[2]);
+$(".answer4").text(answers[3]);
+
+ 
+ 
+
+
+
+ 
+
+  
