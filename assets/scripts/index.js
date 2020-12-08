ecbd16b9f6caecbe92464d204282d05e0402b07d
rhysseddon <rhysseddon@hotmail.com>
2020-12-08 11:47:27 +0000

    Add nextFlag function and add to resetAnswers function so it adds in current flag then shuffles again

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,52 +1,61 @@
-
-// This code is for start pop up modal
 $(document).ready(function() {
 $("#startModal").modal('show');
 resetAnswers();
+resetButtons();
+nextFlag(); 
+});
 
- });
-// Below code shuffles answers array
-
-var answers = ["wales", "scotland", "ireland", "england"];
+// Takes an array of all the countries and shuffles
+var countries = ["wales", "scotland", "ireland", "england", "france", "germany", "argentina", "nepal", "peru"];
 function resetAnswers(){
-function shuffleArray(answers) {
-      for (let i = answers.length -1; i > 0; i--) {
+function shuffleArray(countries) {
+      for (let i = countries.length -1; i > 0; i--) {
          j = Math.floor(Math.random() * (i + 1));
-         [answers[i], answers[j]] = [answers[j], answers[i]];
+         [countries[i], countries[j]] = [countries[j], countries[i]];
         }
  }
- // Below code changes answer button text to random answer
-shuffleArray(answers);
-$(".answer1").text(answers[0]);
-$(".answer2").text(answers[1]);
-$(".answer3").text(answers[2]);
-$(".answer4").text(answers[3]);
+ // Takes 3 answers from the shuffled array and keeps shuffling if the answers
+do {
+    shuffleArray(countries);
+shuffledArray = [countries[0],countries[1], countries[2]];
+console.log("shuffling" + shuffledArray );
+} while (shuffledArray.includes(flags[flagCount]))
+console.log(shuffledArray);
+shuffledArray.push(flags[flagCount]);
+console.log(shuffledArray);
+shuffleArray(shuffledArray); // Reshuffle array 
+console.log(shuffledArray);
+$(".answer1").text(shuffledArray[0]);
+$(".answer2").text(shuffledArray[1]);
+$(".answer3").text(shuffledArray[2]);
+$(".answer4").text(shuffledArray[3]);
+}
+
+var flags = ["wales", "scotland", "ireland", "england", "ireland", "france"];
+var flagCount = 0;
+var answer;
+
+$('.answer-button').on('click', function(answer) {
+ answer = flags[flagCount];
+    if ($(this).text().match(answer)) {
+     console.log(answer + " correct");
+     flagCount ++;
+     resetAnswers();
+     resetButtons();
+     nextFlag();
+ } else {
+     console.log("incorrect")
+ }
+});
+
+function nextFlag() {
+    $(".flag").attr("src", "assets/images/" + flags[flagCount] + "-flag.jpg");
 }
 
 function resetButtons() {
     $('.answer-button').css("background-color", "#df9a57");
 }
 
-function askQuestion(flag) {
- $(".flag").attr("src", "assets/images/" + flag + "-flag.jpg");
-$('.answer-button').on('click', function() {
-if ($(this).text().match(flag)) {
-$(this).css("background-color", "green");
- setTimeout( function() {
-resetAnswers();
-resetButtons();
-}, 2000);
-} else {
-    $(this).css("background-color", "red");
-   }
-return;
-});
-}
-
-
-
-
-askQuestion("wales");
 
 
 
