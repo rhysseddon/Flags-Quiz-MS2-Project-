c53b2e8409c24348e17c94ee39896de5496b0d44
rhysseddon <rhysseddon@hotmail.com>
2020-12-08 12:14:35 +0000

    Add timeout function to display button green when correct and add display button red when incorrect

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,51 +1,56 @@
-$(document).ready(function() {
-$("#startModal").modal('show');
-resetAnswers();
-resetButtons();
-nextFlag(); 
+$(document).ready(function () {
+    $("#startModal").modal('show');
+    resetAnswers();
+    resetButtons();
+    nextFlag();
 });
 
 // Takes an array of all the countries and shuffles
 var countries = ["wales", "scotland", "ireland", "england", "france", "germany", "argentina", "nepal", "peru"];
-function resetAnswers(){
-function shuffleArray(countries) {
-      for (let i = countries.length -1; i > 0; i--) {
-         j = Math.floor(Math.random() * (i + 1));
-         [countries[i], countries[j]] = [countries[j], countries[i]];
+
+function resetAnswers() {
+    function shuffleArray(countries) {
+        for (let i = countries.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            [countries[i], countries[j]] = [countries[j], countries[i]];
         }
- }
- // Takes 3 answers from the shuffled array and keeps shuffling if the answers
-do {
-    shuffleArray(countries);
-shuffledArray = [countries[0],countries[1], countries[2]];
-console.log("shuffling" + shuffledArray );
-} while (shuffledArray.includes(flags[flagCount]))
-console.log(shuffledArray);
-shuffledArray.push(flags[flagCount]);
-console.log(shuffledArray);
-shuffleArray(shuffledArray); // Reshuffle array 
-console.log(shuffledArray);
-$(".answer1").text(shuffledArray[0]);
-$(".answer2").text(shuffledArray[1]);
-$(".answer3").text(shuffledArray[2]);
-$(".answer4").text(shuffledArray[3]);
+    }
+    // Takes 3 answers from the shuffled array and keeps shuffling if the answers
+    do {
+        shuffleArray(countries);
+        shuffledArray = [countries[0], countries[1], countries[2]];
+        console.log("shuffling" + shuffledArray);
+    } while (shuffledArray.includes(flags[flagCount]))
+    console.log(shuffledArray);
+    shuffledArray.push(flags[flagCount]);
+    console.log(shuffledArray);
+    shuffleArray(shuffledArray); // Reshuffle array 
+    console.log(shuffledArray);
+    $(".answer1").text(shuffledArray[0]);
+    $(".answer2").text(shuffledArray[1]);
+    $(".answer3").text(shuffledArray[2]);
+    $(".answer4").text(shuffledArray[3]);
 }
 
 var flags = ["wales", "scotland", "ireland", "england", "ireland", "france"];
 var flagCount = 0;
 var answer;
 
-$('.answer-button').on('click', function(answer) {
- answer = flags[flagCount];
+$('.answer-button').on('click', function (answer) {
+    answer = flags[flagCount];
     if ($(this).text().match(answer)) {
-     console.log(answer + " correct");
-     flagCount ++;
-     resetAnswers();
-     resetButtons();
-     nextFlag();
- } else {
-     console.log("incorrect")
- }
+        console.log(answer + " correct");
+        $(this).css("background-color", "green");
+        setTimeout(function () {
+            flagCount++;
+            resetAnswers();
+            resetButtons();
+            nextFlag();
+        }, 2000);
+    } else {
+        console.log("incorrect");
+        $(this).css("background-color", "red");
+    }
 });
 
 function nextFlag() {
@@ -125,10 +130,3 @@ return;
 });
 }
 */
-
-
-
-
-
-
-
