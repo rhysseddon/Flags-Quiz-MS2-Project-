0b2e768023279ff873beef7f5dea31954f729fcb
rhysseddon <rhysseddon@hotmail.com>
2020-12-09 16:19:20 +0000

    Move variables to top of page and change var to let on variables and const on array variables, remove all console.log statements

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,50 +1,37 @@
-
-
+const flags = ["wales", "scotland", "ireland", "england", "france", "india", "germany", "usa", "argentina", "chile"];
+let flagCount = 0;
+let score = 0;
+const countries = ["wales", "scotland", "ireland", "england", "france", "germany", "argentina", "usa", "chile", "india"];
 // Takes an array of all the countries and shuffles
-var countries = ["wales", "scotland", "ireland", "england", "france", "germany", "argentina", "usa", "chile", "india"];
 function shuffleArray(countries) {
-        for (let i = countries.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * (i + 1));
-            [countries[i], countries[j]] = [countries[j], countries[i]];
-        }
+    for (let i = countries.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        [countries[i], countries[j]] = [countries[j], countries[i]];
     }
+}
 // Shuffles countries array
 function resetAnswers() {
-    
     // Takes 3 answers from the shuffled countries array and keeps shuffling until it doesnt include the flag.
     do {
         shuffleArray(countries);
         shuffledArray = [countries[0], countries[1], countries[2]];
-        console.log("shuffling" + shuffledArray);
     } while (shuffledArray.includes(flags[flagCount]))
-    console.log(shuffledArray);
     shuffledArray.push(flags[flagCount]); // Adds current flag to array
-    console.log(shuffledArray);
     shuffleArray(shuffledArray); // Reshuffles array 
-    console.log(shuffledArray);
     $(".answer1").text(shuffledArray[0]); // Adds countries to the buttons text
     $(".answer2").text(shuffledArray[1]);
     $(".answer3").text(shuffledArray[2]);
     $(".answer4").text(shuffledArray[3]);
 }
 
-var flags = ["wales", "scotland", "ireland", "england", "france", "india", "germany", "usa", "argentina", "chile"];
-var flagCount = 0;
-
-var score = 0;
-
 // if clicked button text matches flag it turns clicked button green if not turns clicked button red as well as the button containing the text from answer green.
-
-
-
 function playGame() {
-       $(".answer-button").attr("disabled", true);
-   let answer = flags[flagCount];
-    
+    $(".answer-button").attr("disabled", true);
+    let answer = flags[flagCount];
+
     if ($(this).text().match(answer)) {
-        console.log(answer + " correct");
         $(this).css("background-color", "green");
-        
+
         setTimeout(function () {
             flagCount++;
             score++;
@@ -57,8 +44,7 @@ function playGame() {
     } else {
         console.log("incorrect");
         $(this).css("background-color", "red");
-        
-       $('.answer-button:contains("' + answer + '")').css("background-color", "green"); //turns correct answer button green
+        $('.answer-button:contains("' + answer + '")').css("background-color", "green"); //turns correct answer button green
         setTimeout(function () {
             flagCount++;
             $('.flag-count').text("Flag: " + flagCount + "/10");
@@ -68,9 +54,6 @@ function playGame() {
         }, 2000);
     }
 }
- 
-
-
 
 function nextFlag() {
     $(".flag").attr("src", "assets/images/" + flags[flagCount] + "-flag.jpg");
